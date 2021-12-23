81629a2759df4f7d669c8fb4bfd6277e812b6e44
Coder J <buildingguy@sbcglobal.net>
2021-12-22 18:20:05 -0800

    store seacrh inputs

---
+++ assets/js/secondary-script.js
@@ -0,0 +1,14 @@
+function store() {
+    var newData = document.getElementById('input').value;
+
+    if(localStorage.getItem('data') === null) {
+        localStorage.setItem('data', '[]');
+    }
+
+    var oldData = JSON.parse(localStorage.getItem('data'));
+    oldData.push(newData);
+
+    localStorage.setItem('data', JSON.stringify(oldData));
+
+    console.log(localStorage);
+}
